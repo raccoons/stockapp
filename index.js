ae35a28678e53ca01e8b6cd4cb69b4cb54d4d85c
Joe Rodgers <joerodgers89@gmail.com>
2023-10-17 15:00:46 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const { engine } = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+// Set Handlebars Middleware
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+const otherStuff = "Hello more stuff";
+// Set handlebar routes
+app.get('/', (req, res) => {
+    res.render('home', {
+        stuff: otherStuff
+    });
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log(`Server listing on port ${PORT}`));
+
