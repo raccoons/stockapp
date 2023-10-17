da233c18e9c85a79cf0c8711cf8ef4b5c4344e39
Joe Rodgers <joerodgers89@gmail.com>
2023-10-17 17:00:07 +0100

    Actually added stock content

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ const express = require('express');
 const app = express();
 const { engine } = require('express-handlebars');
 const path = require('path');
+const request = require('request');
 
 const PORT = process.env.PORT || 5000;
+
+// API KEY: pk_e1a3792c57f244dbb3384af1fd202a44
+// create call_api function
+function call_api(finishedAPI) {
+    request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_e1a3792c57f244dbb3384af1fd202a44', { json: true }, (err, res, body) => {
+    if (err) {return console.log(err);}
+    if (res.statusCode === 200) {
+        //console.log(body);
+        finishedAPI(body);
+    };
+});
+
+}
+
+
 
 // Set Handlebars Middleware
 app.engine('handlebars', engine());
@@ -15,9 +31,16 @@ app.set('views', './views');
 const otherStuff = "Hello more stuff";
 // Set handlebar routes
 app.get('/', (req, res) => {
-    res.render('home', {
-        stuff: otherStuff
+    call_api(function(doneAPI) {
+        res.render('home', {
+            stock: doneAPI
+        });
     });
+});
+
+// create about page route
+app.get('/about.html', (req, res) => {
+    res.render('about');
 });
 
 // Set static folder
